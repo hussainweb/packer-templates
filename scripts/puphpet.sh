0039b887d0d5dcdbbdd351fae716e661fec9fd21
hw <hussainweb@gmail.com>
2015-02-17 12:19:07 +0530

    Update template and scripts from latest changes to https://github.com/puphpet/packer-templates

--- scripts/puphpet.sh
+++ scripts/puphpet.sh
@@ -5,10 +5,10 @@ useradd -g www-data www-data
 
 mkdir -p '/.puphpet-stuff/shell'
 
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/os-detect.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh'
-wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate  -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.github.com/puphpet/puphpet/master/src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-puppet.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/os-detect.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/os-detect.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/initial-setup.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/initial-setup.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-ruby.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/install-ruby.sh'
+wget --quiet --tries=5 --connect-timeout=10 --no-check-certificate -O '/.puphpet-stuff/shell/install-puppet.sh' 'https://raw.githubusercontent.com/puphpet/puphpet/master/archive/puphpet/shell/install-puppet.sh'
 
 chmod +x '/.puphpet-stuff/shell/os-detect.sh'
 chmod +x '/.puphpet-stuff/shell/initial-setup.sh'
@@ -18,3 +18,5 @@ chmod +x '/.puphpet-stuff/shell/install-puppet.sh'
 source '/.puphpet-stuff/shell/initial-setup.sh' '/.puphpet-stuff'
 source '/.puphpet-stuff/shell/install-ruby.sh'
 source '/.puphpet-stuff/shell/install-puppet.sh'
+
+apt-get -y install bindfs
